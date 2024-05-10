b609b38466cb9029f575a227896d4f922a7a1a1f
Grace Yap <grace.u.yap@gmail.com>
2024-05-10 11:46:31 +0800

    12 Conclusion

---
+++ exercise_files/12_notes.py
@@ -0,0 +1,23 @@
+### DATA SCIENCE
+# data science library:
+# https://scikit-learn.org/stable/
+# data structures from panda or the python data analysis library:
+# http://pandas.pydata.org/
+
+### Flask for easy REST APIs
+# flask makes it fast and easy to build scalable REST APIs
+# the project is very intuitive and well-documented, and they make it really easy to plug in with other libraries to build enterprise code
+# https://flask.palletsprojects.com/en/2.1.x/
+
+### WEB SCRAPING
+# use python to build powerful scrapers and bots to trawl the internet 
+# the python request library is one of the best and easiest HTTP request libraries
+# python's ability to integrate with web browser drivers like selenium is unparalleled
+# https://www.selenium.dev/
+
+### favorite_python_resources.pdf
+# - information on LinkedIn courses recommendations
+
+### NEXT STEP
+# Python Challenge Project
+# https://www.linkedin.com/learning/python-practice-real-world-coding-challenges?u=2201186
